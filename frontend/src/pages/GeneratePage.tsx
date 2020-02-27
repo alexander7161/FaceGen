024863b36aa1a2d17af38e5ab508986034de03c0
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-02-27 13:48:16 +0000

    Add bottom margin to generate page

--- frontend/src/pages/GeneratePage.tsx
+++ frontend/src/pages/GeneratePage.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import Faces from "../components/FacesList";
 import GenerateFaceButton from "../components/GenerateFaceButton";
 import { Container } from "@material-ui/core";
+import styled from "styled-components";
+
+const StyledContainer = styled(Container)`
+  margin-bottom: 64px;
+`;
 
 const GeneratePage = () => {
   return (
-    <Container maxWidth="md">
+    <StyledContainer maxWidth="md">
       <Faces />
       <GenerateFaceButton />
-    </Container>
+    </StyledContainer>
   );
 };
 
