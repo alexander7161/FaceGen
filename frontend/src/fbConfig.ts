5f7db4d3c3e95426297052f5c0af1660e8ca7b69
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-02-29 15:05:45 +0000

    add ts-jest

--- frontend/src/fbConfig.ts
+++ frontend/src/fbConfig.ts
@@ -2,7 +2,7 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
 import "firebase/auth";
-import config from "../../firebase-config.json";
+import config from "./firebase-config.json";
 
 const firebaseApp = firebase.initializeApp(config);
 
