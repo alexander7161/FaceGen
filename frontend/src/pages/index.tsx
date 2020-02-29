98fcffd906059ec5b3fd4dad7cd202e49b10d9e4
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-02-29 14:39:18 +0000

    frontend test

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import HomePage from "./HomePage";
 import { Route, Switch } from "react-router-dom"; // react-router v4/v5
+import HomePage from "./HomePage";
 import GeneratePage from "./GeneratePage";
 
 const Pages = () => {
