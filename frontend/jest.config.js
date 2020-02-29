35e5f238ee975b623065aeb5ffe382bbc44d0298
Alexander Davis <alexanderdavis.net@googlemail.com>
2020-02-29 19:45:26 +0000

    Revert "rename pages/index"
    This reverts commit 5a7c665457f7afbbfea3f3686ac8af16343801f4.

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,8 @@
-const { defaults } = require("jest-config");
-
 module.exports = {
   roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts", "tsx"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
 };
